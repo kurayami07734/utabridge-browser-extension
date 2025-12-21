430c672213ba4c4d93ac8505cf3e5e5a55f0d8d6
Aditya Ghidora <adityaghidora@gmail.com>
2025-12-21 19:42:52 +0530

    chore: Tear down placeholder UI

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,34 +1,9 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-semibold text-center text-gray-800">WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <div className="bg-black flex justify-center content-center">
+      <h1 className="text-3xl font-semibold text-center text-white">UtaBridge</h1>
+    </div>
   );
 }
 
