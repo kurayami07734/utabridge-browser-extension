d19753926f7dfea48379bdb4269fd0490ecddce8
Aditya Ghidora <adityaghidora@gmail.com>
2026-02-08 19:44:53 +0530

    chore: Separate manifest options by browser

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,23 +1,35 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, UserManifest } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     srcDir: 'src',
     modules: ['@wxt-dev/module-react'],
-    manifest: {
-        name: 'UtaBridge',
-        description: 'Seamlessly translate and romanize Japanese song titles on Spotify.',
-        version: '0.1.0',
-        short_name: 'UtaBridge',
-        permissions: ['storage'],
-        host_permissions: ['*://open.spotify.com/*'],
-        browser_specific_settings: {
-            gecko: {
-                id: 'adityaghidora+utabridge@gmail.com',
-                strict_min_version: '109.0',
-            },
-        },
+    manifest: ({ browser }) => {
+        const config: UserManifest = {
+            name: 'UtaBridge',
+            description: 'Seamlessly translate and romanize Japanese song titles on Spotify.',
+            version: '0.1.0',
+            short_name: 'UtaBridge',
+            permissions: ['storage'],
+            host_permissions: ['https://open.spotify.com/*'],
+        };
+
+        if (browser === 'chrome') {
+            config.oauth2 = {
+                client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+                scopes: ['openid', 'email', 'profile'],
+            };
+        } else if (browser === 'firefox') {
+            config.browser_specific_settings = {
+                gecko: {
+                    id: 'adityaghidora+utabridge@gmail.com',
+                    strict_min_version: '109.0',
+                },
+            };
+        }
+
+        return config;
     },
     vite: () => ({
         plugins: [tailwindcss()],
